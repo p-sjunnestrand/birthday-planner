eafb5eb9ca255aa7b0d8abf8404a7109ff5b2d95
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-04-19 00:00:12 +0200

    reworked listmaking and started w form for adding new items

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 let mainWrp = document.getElementById("mainWrp");
+//temporary person list
+const persons = [
+    {
+        person: "Petter",
+        items: 4
+    },
+    {
+        person: "Norah",
+        items: 0
+    },
+    {
+        person: "David",
+        items: 3
+    },
+    {
+        person: "Ronja",
+        items: 3
+    },
+    {
+        person: "My",
+        items: 0
+    },
+    {
+        person: "Martin",
+        items: 0
+    },
+    {
+        person: "Alvin",
+        items: 0
+    },
+    {
+        person: "Meja",
+        items: 0
+    },
+    {
+        person: "Tove",
+        items: 0
+    },
+    {
+        person: "Tomas",
+        items: 0
+    },
+    {
+        person: "Stella",
+        items: 0
+    },
+    {
+        person: "Margareta",
+        items: 0
+    },
+    {
+        person: "Håkan",
+        items: 0
+    },
+    {
+        person: "Lars-Erik",
+        items: 0
+    }
+];
 
-const persons = ["Petter", "Norah", "Ronja", "David", "My", "Martin", "Alvin", "Meja", "Tove", "Tomas", "Stella", "Margareta", "Håkan", "Lars-Erik"]
 //Temporary item list
 let wishListItems = [
     {
@@ -61,25 +119,27 @@ function mainPage (){
     wishList.id = "wishList";
 
     const addListBtn = document.createElement('button');
-    addListBtn.classList.add('addListBtn');
+    addListBtn.id ='addListBtn';
     addListBtn.innerText = "Lägg till nya önskemål";
     wishListWrp.appendChild(addListBtn);
 
     //populates wishList area with empty lists
     persons.forEach(person => {
-        // console.log(person);
-        let wishListItem = document.createElement('article');
-        wishListItem.classList.add('wishListItem');
-        
-        wishList.appendChild(wishListItem);
-        const listItemHeader = document.createElement('h3');
-        listItemHeader.classList.add('listItemHeader');
-        listItemHeader.innerHTML = person;
-        wishListItem.appendChild(listItemHeader);
-        let wishListItemUl = document.createElement('ul');
-        wishListItemUl.id = person;
-        wishListItemUl.classList.add('wishListItemUl');
-        wishListItem.appendChild(wishListItemUl); 
+        if(person.items > 0){
+            // console.log(person);
+            let wishListItem = document.createElement('article');
+            wishListItem.classList.add('wishListItem');
+            
+            wishList.appendChild(wishListItem);
+            const listItemHeader = document.createElement('h3');
+            listItemHeader.classList.add('listItemHeader');
+            listItemHeader.innerHTML = person.person;
+            wishListItem.appendChild(listItemHeader);
+            let wishListItemUl = document.createElement('ul');
+            wishListItemUl.id = person.person;
+            wishListItemUl.classList.add('wishListItemUl');
+            wishListItem.appendChild(wishListItemUl); 
+        }
     });
     addLists();
 }
@@ -91,4 +151,52 @@ function addLists(){
         // console.log(listItemAdd);
         listItemAdd.insertAdjacentHTML('beforeend', `<li>${item.item}</li>`);
     })
+}
+document.getElementById('addListBtn').addEventListener('click', () => {
+    console.log('hej');
+    loadListForm();
+
+})
+//Loads a form for adding items and lists to wish list
+function loadListForm () {
+    const fadedBackdrop = document.createElement('div');
+    fadedBackdrop.id = 'fadedBackdrop';
+    document.body.insertAdjacentElement("afterbegin", fadedBackdrop);
+
+    const addListFormWrp = document.createElement('article');
+    addListFormWrp.id = 'addListFormWrp'
+    fadedBackdrop.appendChild(addListFormWrp);
+
+    const addListForm = document.createElement('form');
+    addListFormWrp.appendChild(addListForm);
+    addListForm.insertAdjacentHTML('beforeend', `<label for = "selectPerson">Person:</label><br>`)
+
+    const selectPerson = document.createElement('select');
+    selectPerson.id = 'selectPerson';
+
+    selectPerson.insertAdjacentHTML("beforeend", `<option>--Välj person--</option>`);
+
+    // const choosePersonPrompt = document.createElement('option');
+    // choosePersonPrompt.textContent = "--Välj person--";
+    // selectPerson.appendChild(choosePersonPrompt);
+
+    persons.forEach(person => {
+        selectPerson.insertAdjacentHTML('beforeend', `<option value = "${person.person}">${person.person}</option>`);
+    });
+    addListForm.appendChild(selectPerson);
+
+    addListForm.insertAdjacentHTML('beforeend', `<br><label for = "presentInput">ösnkar sig:</label><br>`);
+    const presentInput = document.createElement('input');
+    presentInput.id = "presentInput";
+    addListForm.appendChild(presentInput);
+
+    addListForm.insertAdjacentHTML('beforeend', `<br><label for = "presentURL">Länk till ev. webbshop/hemsida för produkten:</label><br>`);
+    const presentURL = document.createElement('input');
+    presentURL.id = "presentURL";
+    addListForm.appendChild(presentURL);
+
+    const submitBtn = document.createElement("button");
+    submitBtn.id = "submitBtn";
+    submitBtn.textContent = "Lägg till";
+    addListForm.appendChild(submitBtn);
 }
