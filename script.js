77dac9a62080b9e9558b361b7f460944e6cbf6a1
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-04-29 23:35:29 +0200

    post connection w backend working. Add form basic done

--- script.js
+++ script.js
@@ -159,17 +159,29 @@ document.getElementById('addListBtn').addEventListener('click', () => {
 })
 //Loads a form for adding items and lists to wish list
 function loadListForm () {
+    //adds faded backdrop to shift focus to form
     const fadedBackdrop = document.createElement('div');
     fadedBackdrop.id = 'fadedBackdrop';
     document.body.insertAdjacentElement("afterbegin", fadedBackdrop);
 
+    //adds div in which form is placed
     const addListFormWrp = document.createElement('article');
     addListFormWrp.id = 'addListFormWrp'
     fadedBackdrop.appendChild(addListFormWrp);
 
+    //adds btn for closing form window
+    const closeFormWindow = document.createElement('button');
+    closeFormWindow.id = 'closeFormWindow';
+    closeFormWindow.innerText = 'X';
+    addListFormWrp.appendChild(closeFormWindow);
+
+    closeFormWindow.addEventListener('click', () =>{
+        fadedBackdrop.remove();
+    })
+
     const addListForm = document.createElement('form');
     addListFormWrp.appendChild(addListForm);
-    addListForm.insertAdjacentHTML('beforeend', `<label for = "selectPerson">Person:</label><br>`)
+    addListForm.insertAdjacentHTML('beforeend', `<label for = "selectPerson">Person:</label>`);
 
     const selectPerson = document.createElement('select');
     selectPerson.id = 'selectPerson';
@@ -191,10 +203,20 @@ function loadListForm () {
     // presentInput.attributes.required = "required";
     addListForm.appendChild(presentInput);
 
+    addListForm.insertAdjacentHTML('beforeend', `<br><label for = "presentDesc">Ev. beskrivning av önskemålet</label><br>`);
+    const presentDesc = document.createElement('textarea');
+    presentDesc.id = "presentDesc";
+    addListForm.appendChild(presentDesc);
+
     addListForm.insertAdjacentHTML('beforeend', `<br><label for = "presentURL">Länk till ev. webbshop/hemsida för produkten:</label><br>`);
     const presentURL = document.createElement('input');
     presentURL.id = "presentURL";
     addListForm.appendChild(presentURL);
+
+    addListForm.insertAdjacentHTML('beforeend', `<br><label for = "presentShop">Ev. butik där det önskade kan köpas</label><br>`);
+    const presentShop = document.createElement('input');
+    presentShop.id = "presentShop";
+    addListForm.appendChild(presentShop);
 
     const submitBtn = document.createElement("button");
     submitBtn.id = "submitBtn";
@@ -204,8 +226,49 @@ function loadListForm () {
     document.getElementById('submitBtn').addEventListener('click', evt =>{
         evt.preventDefault();
         console.log('click');
-        if (selectPerson.value == ''){
-            console.log('Välj en person!');
+        if (selectPerson.value === ''){
+            console.log('Välj person och önskemål!');
+            addListFormWrp.insertAdjacentHTML('beforeend', `<div class = "warning">Välj en person och skriv något hen önskar sig!</div>`);
+            selectPerson.style.border = '3px solid red';
+            presentInput.style.border = '3px solid red';
+        } else {
+            const postedItem = {
+                "person": selectPerson.value,
+                "item": {
+                    "item_name": presentInput.value,
+                    "item_desc": presentDesc.value,
+                    "item_url": presentURL.value,
+                    "item_shop": presentShop.value
+                }
+            }
+            console.log(postedItem);
+            fetch('http://localhost:3000/users/', {
+                method: 'post',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(postedItem)
+            })
+            .then(result => result.json())
+            .then(data => {
+                console.log(data);
+                if(data.status !== "added"){
+                    itemNotPosted();
+                } else {
+                    itemPosted(presentInput.value, selectPerson.value);
+                }
+            })
         }
     });
+}
+
+function itemNotPosted(){
+    document.getElementById('addListFormWrp').insertAdjacentHTML('beforeend', `<div class = "warning">Något gick fel. Prova att lägga till önskemålet igen. Om det inte fungerar, prova att ladda om sidan.</div>`);
+}
+function itemPosted(present, person) {
+    document.getElementById('addListFormWrp').insertAdjacentHTML('beforeend', `<div>${present} tillagd ${person}s önskelista!</div>`);
+    presentInput.value = '';
+    presentDesc.value = '';
+    presentURL.value = '';
+    presentShop.value = '';
 }
